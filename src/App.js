f1908e18713e9b7f5c34fae63dc67cfeab0b0c2d
mcordobaf <mcordobaf@gmail.com>
2020-11-06 01:23:29 -0300

    prueba de cambio de pool de cognito
    prueba de cambio de pool de cognito

--- src/App.js
+++ src/App.js
@@ -4,7 +4,29 @@ import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
+
+Amplify.configure({
+  Auth: {
+
+      // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+      identityPoolId: 'us-east-1:c470cd15-1c60-4dd9-9cbb-9404a22b2a15',
+      // REQUIRED - Amazon Cognito Region
+      region: 'us-east-1',
+      identityPoolRegion: 'us-east-1',
+      userPoolId: 'us-east-1_gW0emGvaU',
+      userPoolWebClientId: '3mh4o9mno1ml28l0faal77qv55',
+      mandatorySignIn: false,
+      storage: MyStorage,
+      aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS",
+      oauth: {
+          //domain: 'your_cognito_domain',
+          //scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
+          //redirectSignIn: 'http://localhost:3000/',
+          //redirectSignOut: 'http://localhost:3000/',
+          //responseType: 'code' // or 'token', note that REFRESH token will only be generated when the responseType is code
+      }
+  }
+});
 
 class App extends Component {
   render() {
